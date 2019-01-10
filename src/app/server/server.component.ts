a29b67d15bdb186afd227d976df73a8ad9137f3a
Muhammed Faheem <muhammed.faheem@softwaysolutions.com>
2019-01-10 13:22:50 +0530

    update: Applying CSS Classes Dynamically with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.scss']
 
 })
 export class ServerComponent {
@@ -13,7 +14,7 @@ export class ServerComponent {
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }
 
-    getServerStatus(){
+    getServerStatus() {
         return this.serverStatus;
     }
 
